fb1e223e68cf540f83f8cb4c43dbf11171951b9d
Gerry Waterston <gerry.waterston@bemo.co>
2025-01-01 13:47:23 +0000

    Lesson 44 - retrieves the login token

--- api-calls/getLoginToken.js
+++ api-calls/getLoginToken.js
@@ -1,10 +1,22 @@
+//  Import the dependency that translates JSON to JS
 import * as nodeFetch from "node-fetch"
 
 export const getLoginToken = async () => {
+    // Store token, method & payload in "reponse" 
     const response = await nodeFetch("http://localhost:2221/api/login", {
+        // From Network > Headers > Request Method
         method: "POST",
-        body: { username: "admin", password: "Admin123" }
-
+        // From Network > Payload
+        // Use JSON.stringify to convert body to a string
+        body: JSON.stringify({ username: "admin", password: "Admin123" })
     })
+    // Error handling if call not worked
+    if (response.status !== 200) {
+        throw new Error("Error occurred trying to retrieve the login token.")
+    }
+    // store all translated response data as a json object
+    const body = await response.json()
+    // It now contains the token property and can be returned as body.token
+    return body.token
 
 }
--- page-objects/MyAccountPage.js
+++ page-objects/MyAccountPage.js
@@ -6,7 +6,7 @@ export class MyAccountPage {
 
     visit = async () => {
         await this.page.goto("/my-account")
-        await this.page.pause()
+        // await this.page.pause()
     }
 
 }
