c2c5904f42e14099541f35f62aea841ff16ddeb7
Gerry Waterston <gerry.waterston@bemo.co>
2025-01-01 13:18:12 +0000

    Lesson 43 - sets up the method to get the login token and creates an api-calls folder

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,7 +1,12 @@
 import { test } from "@playwright/test"
 import { MyAccountPage } from "../page-objects/MyAccountPage.js"
+import { getLoginToken } from "../api-calls/getLoginToken.js"
 
 test.only("My Account using cookie injection", async ({ page }) => {
+    // Make a request for the login token
+    const loginToken = await getLoginToken()
+    console.warn({loginToken})
+    // Inject login token into the browser
     const myAccount = new MyAccountPage(page)
     await myAccount.visit()
 
