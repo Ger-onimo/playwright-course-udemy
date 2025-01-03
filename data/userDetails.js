8af14da8317655104b2ab59499b71bf7da819b08
Gerry Waterston <gerry.waterston@bemo.co>
2025-01-03 14:39:02 +0000

    Lesson 47 - creates an environment variable for password. Creates a .env file. Installs the dotenv dependency.

--- data/userDetails.js
+++ data/userDetails.js
@@ -1,4 +1,8 @@
 export const adminDetails = {
     username: "admin",
-    password: "Admin123"
+    password: process.env.ADMIN_PASSWORD
 }
+
+// process.env stores external env data such as password
+// Create a .env.js file to store the ADMIN_PASSWORD variable
+// This should be secret, so is then "ignored" in .gitignore.js
