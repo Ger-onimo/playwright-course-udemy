d82ec701a3040414a07321ae6458a04464909338
Gerry Waterston <gerry.waterston@bemo.co>
2025-01-03 12:54:13 +0000

    Lessons 44 & 45 - completes retrieving token and injecting into the browser.

--- page-objects/MyAccountPage.js
+++ page-objects/MyAccountPage.js
@@ -1,12 +1,18 @@
+import { expect } from "@playwright/test"
+
 export class MyAccountPage {
     constructor(page) {
         this.page = page
 
+        this.pageHeading = page.getByRole('heading', { name: 'My Account' })
     }
 
     visit = async () => {
         await this.page.goto("/my-account")
-        // await this.page.pause()
+    }
+
+    waitForPageHeading = async () => {
+        await this.pageHeading.waitFor()
     }
 
 }
--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -9,5 +9,13 @@ test.only("My Account using cookie injection", async ({ page }) => {
     // Inject login token into the browser
     const myAccount = new MyAccountPage(page)
     await myAccount.visit()
-
+    // injecting cookies
+    await page.evaluate((loginTokenInsideBrowserCode) => {
+        document.cookie = "token=" + loginTokenInsideBrowserCode // using instead of loginToken as that is outside this function's code. 
+    }, [loginToken]) // loginToken is used as the second argument and somehow this works!!
+    await myAccount.visit()
+    // Effectively this is going via the login page, retrieving the cookie so that the page uses
+    // the same data to return the user to the my-account page - on a refresh for example
+    await myAccount.waitForPageHeading()
 })
+
